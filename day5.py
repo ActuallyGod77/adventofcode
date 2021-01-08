cc86fc5922978a4d1a6d1b323196ab03f6d9bc73
ActuallyGod77 <76794598+ActuallyGod77@users.noreply.github.com>
2021-01-08 15:55:29 +0000

    Add files via upload

--- day5.py
+++ day5.py
@@ -9,41 +9,25 @@ while len(content) > x + 1:
     x += 1
     ticket = split(content[x])
     y = -1
-    row = 128
+    row = 127
     power = 7
     while len(ticket) - 3 > y + 1:
         y += 1
         if ticket[y] == 'F':
-            if y == 0:
-                power -= 1
-                row = (row - (2**power)) - 1
-            else:
-                power -= 1
-                row = (row - (2**power)) 
-        elif ticket[y] == 'B':
-            if y == 0:
-                row -= 1
-                power -= 1
-            else:
-                power -= 1
+            power -= 1
+            row = (row - (2**power)) 
+        else:
+            power -= 1
     z = 6
-    column = 8
+    column = 7
     count = 3
     while len(ticket) > z + 1:
         z += 1
         if ticket[z] == 'L':
-            if z == 7:
-                count -= 1
-                column = (column - (2**count)) - 1
-            else:
-                count -=1
-                column = (column - (2**count))
-        elif ticket[z] == 'R':
-            if z == 7:
-                column -= 1
-                count -= 1
-            else:
-                count -= 1
+            count -=1
+            column = (column - (2**count))
+        else:
+            count -= 1
     if 0 < row < 127:
         ID.append((row * 8) + column)
 #highest_ID = max(ID)
@@ -51,10 +35,7 @@ while len(content) > x + 1:
 check = -1
 while len(ID) > check + 1:
     check += 1
-    if (ID[check] - 2) in ID and (ID[check] - 1) not in ID:
-        print('Your seat ID: ' +str(ID[check]-1))
-        break
-    elif (ID[check] + 2) in ID and (ID[check] + 1) not in ID:
+    if (ID[check] + 2) in ID and (ID[check] + 1) not in ID:
         print('Your seat ID: ' +str(ID[check]+1))
         break
     else:
